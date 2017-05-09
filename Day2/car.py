d6b03cb9c3ab0b94bb95edf554ef71f4dc636e7b
ledduh <ledduh@gmail.com>
2017-05-10 00:20:39 +0300

    correct indentation issues

--- Day2/car.py
+++ Day2/car.py
@@ -1,35 +1,39 @@
-def Car(self):
-	def __init__( self, name='General', model = 'GM', car_type= None, speed = 0):#function is initialized
+class Car(object):
+
+    #function is initialized
+	def __init__( self, name='General', model = 'GM', car_type='saloon', speed = 0):
 		self.name=name
 		self.model = model
 		self.speed = speed
-		self.Car_type = Car_type
-
-		if self.name == 'Toyota' or self.name=='Rover': #if the  car name is toyota or rover it has 2 doors
+		self.car_type = car_type
+        
+        #if the  car name is toyota or rover it has 2 doors
+		if self.name == 'Porsche' or self.name=='Koenigsegg': 
 			self.num_of_doors = 2
 		else:
 			self.num_of_doors = 4 #Other cars have four doors
+		#if the car type is a trailer it has 8 wheels
+			
+		if self.car_type =='trailer': 
+			self.num_of_wheels = 8
+		else:
+			#other car types have 4 wheels
+			self.num_of_wheels = 4 
 
-			if self.car_type =='trailer': #if the car type is a trailer it has 8 wheels
-				self.num_of_wheels = 8
-			else:
-				self.num_of_wheels = 4 #other car types have 4 wheels
+	#A function is defined to check whether it is a trailer or a saloon
+	def is_saloon(self): 
+		if self.car_type is not 'trailer':
+			self.car_type == 'saloon'
+			return True
+		else:
+			return False
 
-				def is_saloon(self): #A function is defined to check whether it is a trailer or a saloon
-					if self.car_type != 'trailer':
-						self.car_type == 'saloon'
-						return True
-					else:
-						return False
-
-						def drive (self,moving_speed): # A function is defined to check if a car is a trailer it should be moving at a speed of 77
-							if self.car_type =='trailer':
-								self.speed = 77
-							else:
-								pass
-
-								if self.name=='Mercedes': #If the car is named Mercedes it movrd at ten times the moving speed
-									self.speed = 10**moving_speed
-								else:
-									pass
-									return self
+	def drive (self,speed):
+		'''
+		 A function is defined to check if a car is a trailer it should be moving at a speed of 77
+		 '''
+		if self.car_type =='trailer':
+			return 77
+		else:
+			return 10 * speed
+			
