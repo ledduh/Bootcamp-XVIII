d194faed7dbe8f57dd321f9439bbf2f9931c9ca6
ledduh <ledduh@gmail.com>
2017-05-09 18:16:29 +0300

    Object Oriented Programming

--- Day 2/OOP.py
+++ Day 2/OOP.py
@@ -1,4 +1,4 @@
-class BankAccount(object): #creat a class with function
+class BankAccount(object): 
 
     def __init__(self):
         pass
@@ -10,46 +10,46 @@ class BankAccount(object): #creat a class with function
 
 class SavingsAccount(BankAccount):#class SavingsAccount inherits from BankAccount
 
-    def __init__(self):
+    def __init__(self): #Balance is established first
         self.balance = 500
 
-    def deposit(self, amount):
-        if (amount < 0):
+    def deposit(self, amount): 
+        if (amount < 0): #if the amount being deposited is less than 0 it gives a return of invalid deposit amount
             return "Invalid deposit amount"
         else:
-            self.balance += amount
+            self.balance += amount #if the deposited amount is above 0 it should return the balance 
             return self.balance
 
     def withdraw(self, amount):
         if ((self.balance - amount) > 0) and ((self.balance - amount) < 500):
-            return "Cannot withdraw beyond the minimum account balance"
-        elif (self.balance - amount) < 0:
+            return "Cannot withdraw beyond the minimum account balance" #if the amount in the account is greater than 0 and less than 500 it should return Cannot withdraw beyond the minimum account balance
+        elif (self.balance - amount) < 0: #if amount is less than 0 it returns Cannot withdraw beyond the current account balance
             return "Cannot withdraw beyond the current account balance"
-        elif amount < 0:
+        elif amount < 0: #if the amount in the account is less than o it returns Invalid withdraw amount
             return "Invalid withdraw amount"
         else:
             self.balance -= amount
-            return self.balance 
+            return self.balance #the balance is returned after a withdrawal is made
 
 class CurrentAccount(BankAccount):
 
     def __init__(self):
-        self.balance = 0
+        self.balance = 0 #current account has 0 amount 
 
 
     def deposit(self, amount):
-        if amount < 0:
+        if amount < 0: #if the amount being deposited is less than 0 it gives a return of invalid deposit amount
             return "Invalid deposit amount"
         else:
-            self.balance += amount
+            self.balance += amount #if the deposited amount is above 0 it should return the balance
             return self.balance
 
 
     def withdraw(self, amount):
-        if amount < 0:
+        if amount < 0: #if the amount in the account is less than o it returns Invalid withdraw amount
             return "Invalid withdraw amount"
         elif self.balance < amount:
-            return "Cannot withdraw beyond the current account balance"
+            return "Cannot withdraw beyond the current account balance" #if the balance is less than the amount which is to be withdrawn it returns Cannot withdraw beyond the current account balance
         else:
             self.balance -= amount
-            return self.balance
+            return self.balance #the balance is returned after a withdrawal is made
--- Day 2/fizzbuzz.py
+++ Day 2/fizzbuzz.py
@@ -1,9 +1,9 @@
-def fizz_buzz(number):
-	if number%5==0 and number%3==0:
+def fizz_buzz(number): #A function fizz_buzz is defined which has a parameter of a number
+	if number%5==0 and number%3==0: #A condition is set if the number is divisible by both 5 & 3 it should return FizzBuzz
 		return 'FizzBuzz'
-	elif number%3==0 and number%5!=0:
+	elif number%3==0 and number%5!=0: #if the number is divisible by 3 and not 5 it returns Fizz
 		return 'Fizz'
-	elif number%5==0 and number%3 !=0:
+	elif number%5==0 and number%3 !=0: #If the number is divisible by 5 and not 3 it returns buzz
 		return 'Buzz'
 	else:
-		return number
+		return number #The function should return the number if it does not meet any of the conditions
