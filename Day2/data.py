ad3825fff3cf856b18f43708e93faeac28c3e4e1
ledduh <ledduh@gmail.com>
2017-05-09 22:54:53 +0300

    chore: fixes identation and put a more descriptive variable data

--- Day2/data.py
+++ Day2/data.py
@@ -1,20 +1,33 @@
-def data_type(x):
-	if isinstance (x, basestring): #condition checks to see in the instance given is a string or unicode 
-		return len(x) #it returns the lenght of the given input
-	elif isinstance (x, bool): #if the instance given is a bool it returns it 
-		return x
-	elif x is None: #if the value of x is none it returns no value
+def data_type(data):
+	#condition checks to see in the instance given is a string
+	if isinstance (data, str):  
+	#it returns the lenght of the given input
+		return len(data) 
+    #if the instance given is a bool it returns the data 
+	elif isinstance (data, bool): 
+		return data
+	#if the value of x is none it returns no value
+	elif data is None: 
 		return 'no value'
-	elif isinstance (x, int): #if the instance given is an integer and the value is less than 100 it returns less than 100
-		if x<100:
+	'''
+	if the instance given is an integer and the value is less than 100 it returns less than 100
+	'''
+	elif isinstance (data, int): 
+		if data<100:
 			return 'less than 100'
-		elif x==100: # if the value is 100 it returns equal to 100
+	# if the value is 100 it returns equal to 100
+		elif data==100: 
 			return 'equal to 100'
 		else:
 			return 'more than 100'
-		if isinstance (x, list): #if the instance given is a list and the list is less than 3 it returns none
-			if len(x)<3:
-				return None
-			else:
-				return x[2]
+	'''
+	if the instance given is a list and the list is less than 3 it returns none
+	'''
+	elif isinstance (data, list): 
+		if len(data)<3:
+			return None
+		else:
+
+			return data[2]
 			
+print(data_type([1,2]))
